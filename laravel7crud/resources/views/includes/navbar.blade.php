7588b0592f80df428d60be8955515ee8bf06dd3e
rasdi28 <rasdi.abdulrohmman15120@student.unsika.ac.id>
2021-02-24 17:40:59 +0700

    laravel-rud

--- laravel7crud/resources/views/includes/navbar.blade.php
+++ laravel7crud/resources/views/includes/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-md navbar-dark bg-primary shadow-sm">
     <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}">
-            Laravel
+            <img src="{{asset('img/code.svg')}}" alt="" width="50px">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
@@ -13,6 +13,12 @@
                 <li class="nav-item">
                     <a href="{{ route('blog.index') }}" class="nav-link">
                         Blog
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="{{ route('blog.index') }}" class="nav-link">
+                        Tutorial
                     </a>
                 </li>
 
